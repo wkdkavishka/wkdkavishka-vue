9b5ea00e002440ceb4ad82c4efb8f5eb3f273e8b
WKDKavishka <w.k.d.kavishka@outlook.com>
2024-05-18 17:09:56 +0530

    ADD footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <navbar />
+    <NavBar />
     <router-view />
+    <FooterBar />
   </div>
 </template>
 
 <script setup>
-import navbar from "./components/NavBar.vue";
+import NavBar from "./components/NavBar.vue";
+import FooterBar from "./components/FooterBar.vue";
 </script>
