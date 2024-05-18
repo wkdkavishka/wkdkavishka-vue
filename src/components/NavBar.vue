8a57a1db72991e218c9e86d556cf18627d6e9644
WKDKavishka <w.k.d.kavishka@outlook.com>
2024-05-18 18:06:48 +0530

    FIX navbar allignments and picture size

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,15 +22,22 @@
               >Software Enginear</span
             >
           </div>
+          <div id="download" class="flex justify-center">
+            <a
+              href="#"
+              class="text-center block sm:hidden text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white md:block md:flex-auto my-1"
+              >Download</a
+            >
+          </div>
         </div>
 
         <!-- navigation menu pannel -->
         <div
           id="menu"
           name="menu"
-          class="flex-wrap sm:flex-auto md:flex md:items-center md:w-auto"
+          class="flex-auto flex-wrap md:flex md:items-center md:w-auto sm:w-6/12 px-4"
         >
-          <div class="text-sm sm:block md:block md:flex-grow">
+          <div class="text-sm sm:block md:flex justify-between">
             <a
               href="#responsive-header"
               class="sm:hidden block mt-2 px-2 mr-2 md:inline-block md:mt-0 text-teal-200 hover:text-white hover:bg-teal-700 rounded"
@@ -50,43 +57,38 @@
               Blog
             </a>
           </div>
-          <div id="download">
-            <a
-              href="#"
-              class="block sm:hidden text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0 md:block md:flex-auto"
-              >Download</a
-            >
-          </div>
-          <div>
-            <button
-              id="toggledark"
-              class="sm:hidden h-12 w-12 rounded-md p-2 md:block ali"
-              @click="toggleDark()"
-            >
-              <svg
-                class="fill-violet-700 block dark:hidden"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
-                ></path>
-              </svg>
-              <svg
-                class="fill-yellow-500 hidden dark:block"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
         </div>
         <!-- end -->
+        <!-- theme toggle -->
+
+        <div>
+          <button
+            id="toggledark"
+            class="sm:hidden h-12 w-12 rounded-md p-2 md:block ali"
+            @click="toggleDark()"
+          >
+            <svg
+              class="fill-violet-700 block dark:hidden"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+              ></path>
+            </svg>
+            <svg
+              class="fill-yellow-500 hidden dark:block"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </nav>
     </div>
   </div>
