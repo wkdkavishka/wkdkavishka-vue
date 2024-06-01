278af6d64c97b98fbc4529fd133524e4c84670c3
WKDKavishka <w.k.d.kavishka@outlook.com>
2024-06-01 13:33:45 +0530

    FIX hovered item staying highlighted

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="bg-slate-200 dark:bg-custom-dark">
+  <div
+    class="bg-slate-200 dark:bg-custom-dark"
+    @ionViewWillEnter="resetHoverState"
+  >
     <NavBar class="fixed top-0 w-full z-50" />
     <!-- to fix nav-bar covering the router-view  -->
     <div class="pt-64"></div>
     <div class="pt-4"></div>
+    <div class="pt-64"></div>
     <!--    <page-carousel list="" />-->
     <router-view v-slot="{ Component, route }">
       <transition name="fade" mode="out-in">
@@ -20,6 +24,13 @@ import FooterBar from "./components/MainUse/FooterBar.vue";
 import PageCarousel from "@/components/Small/carousel/PageCarousel.vue";
 
 const _pages = [{ id: 1, page: "/home" }];
+
+function resetHoverState() {
+  const hoveredElements = document.querySelectorAll(".hovered");
+  hoveredElements.forEach((element) => {
+    element.classList.remove("hovered");
+  });
+}
 </script>
 
 <style>
