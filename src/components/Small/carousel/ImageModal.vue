2885f195eae48c1b99df1fc802b58475003a89a2
wkdkavishka <dkmaxhome@gmail.com>
2024-09-07 23:15:14 +0530

    Fix image-view in large screens

--- src/components/Small/carousel/ImageModal.vue
+++ src/components/Small/carousel/ImageModal.vue
@@ -6,7 +6,13 @@
     @click="close"
   >
     <div class="modal-content flex align-middle justify-center" @click.stop>
-      <img :alt="imageUrl" :src="imageUrl" class="modal-image" />
+      <button
+        class="border rounded text-white border-white leading-none hover:border-transparent hover:text-white hover:bg-red-500 md:block md:flex-auto my-1 text-center block text-sm px-4 py-2 close_button"
+        @click="close"
+      >
+        X
+      </button>
+      <img :alt="imageUrl" :src="imageUrl" class="model-image" />
     </div>
   </div>
 </template>
@@ -45,12 +51,18 @@ const close = () => {
   z-index: 1000;
 }
 
-.modal-image {
-  max-width: 96%;
-  max-height: 96%;
-}
-
 .modal-content {
   position: relative;
+  max-width: 50%;
+  /* max-height: 60%; */
+  button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  .modal-image {
+    max-width: 96%;
+    /* max-height: 96%; */
+  }
 }
 </style>
